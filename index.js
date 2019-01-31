a781010f7d33756acf72f9b3addd4ed65df9ee32
Michael Prather <mprather1@gmail.com>
2019-01-30 20:45:20 -0600

    new

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const Shlogger = require('shlogger')
+var { Client } = require('ssh2')
+const config = require('./config.json')
+
+const SOURCE = config.SOURCE
+const OUTPUT = config.OUTPUT
+const HOST = config.HOST
+const USERNAME = config.USERNAME
+const RSA = config.RSA
+
+const logger = new Shlogger()
+var conn = new Client()
+
+conn.connect({
+  host: HOST,
+  port: 22,
+  username: USERNAME,
+  privateKey: require('fs').readFileSync(RSA)
+})
+
+conn.on('ready', function () {
+  conn.exec(`tar -zcvf - ${SOURCE}/*`, function (err, stream) {
+    if (err) { return logger.error(err) }
+    const filename = `${new Date().toISOString()}-backup.tar.gz`
+    const output = fs.createWriteStream(path.join(OUTPUT, filename))
+
+    stream.pipe(output)
+
+    stream.on('close', function (code, signal) {
+      logger.info(`${filename} - written to ${OUTPUT}...`)
+      conn.end()
+    })
+
+    stream.stderr.resume()
+  })
+})
---
+++ readme.md
@@ -0,0 +1,16 @@
+===== mprather1/backup =====
+=== Usage ===
+
+== config.json ==
+
+  {
+    "SOURCE": "/path/to/source/directory",
+    "HOST": "host",
+    "USERNAME": "username",
+    "OUTPUT": "/path/to/output",
+    "RSA": "/path/to/private/key"
+  }
+
+== Run ==
+
+  ./index.js
